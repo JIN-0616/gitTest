796422fe858c12bc034d34a753fc69ce2f425288
KimJungjin <jungjin0616@gmail.com>
2019-02-08 16:50:22 +0900

    tetsing 02

---
+++ GitTest.java
@@ -0,0 +1,28 @@
+package gitTest;
+
+public class GitTest {
+  
+  private String name;
+  
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  private String email;
+  
+  public static void main(String[] args) {
+    
+  }
+}
